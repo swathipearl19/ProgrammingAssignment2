1b3abdca2e4ebc31a205c2dd5cc1625f86006dad
swathipearl19 <swathipearl19@gmail.com>
2016-08-03 14:22:15 -0500

    Updated the code
    Added Cache code along with comments

--- CachenInverse.R
+++ CachenInverse.R
@@ -1,28 +1,50 @@
+## R functions for computing the inverse of a square matrix
+	## and caching the inverse to avoid future recalculation had the matrix
+	## not changed
+	
 
+	## This function creates a special "matrix" object that can cache its inverse.
+	
 
-makeCacheMatrix <- function(x = matrix()) {
-        inv = NULL
-        set = function(y) {
-                x <<- y
-                inv <<- NULL
-        }
-        get = function() x
-        setinv = function(inverse) inv <<- inverse 
-        getinv = function() inv
-        list(set=set, get=get, setinv=setinv, getinv=getinv)
-}
+	makeCacheMatrix <- function(x = matrix()) {
+	  # in
+	  inv_mat <- NULL 
+	  set <- function(y) {
+	    x <<- y
+	    inv_mat <<- NULL
+	  }
+	  
+	  get <- function() x
+	  setinverse <- function(inverse) inv_mat <<- inverse
+	  getinverse <- function() inv_mat
+	  # return the list of all functions
+	  list(set = set, get = get,
+	       setinverse = setinverse,
+	       getinverse = getinverse)
+	  
+	}
+	
 
-cacheSolve <- function(x, ...) {
-        
-        inv = x$getinv()
-               if (!is.null(inv)){
-                message("getting cached data")
-                return(inv)
-        }
-        mat.data = x$get()
-        inv = solve(mat.data, ...)
-        x$setinv(inv)
-        
-        return(inv)
+	
 
-}
+	## This function computes the inverse of the special "matrix" returned by makeCacheMatrix
+	##  above. If the inverse has already been calculated , then the cachesolve should retrieve the inverse from the cache.
+	
+
+	cacheSolve <- function(x, ...) {
+	  ## Return a matrix that is the inverse of 'x'
+	  inv <- x$getinverse()
+	  # checking already calculated in verse and returning if so
+	  if(!is.null(inv)) {
+	    message("getting cached data")
+	    return(inv)
+	  }
+	  
+	  # calculating the inverse if not calculated already or the matrix has changed (was set)
+	  data <- x$get()
+	  inv <- solve(data, ...)
+	  x$setinverse(inv)
+	  inv
+	  
+	}
+
